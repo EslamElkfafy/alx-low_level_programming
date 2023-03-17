179d3e5ba1c8fa7891717a2c9969e7414b22cf7d
EslamElkfafy <eslamadel52@gmail.com>
2023-03-17 07:21:16 -0700

    first commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+	int i;
+	int j;
+
+	for (c = 48; c <= 55; c++)
+	{
+		for (i = c + 1; i <= 56; i++)
+		{
+			for (j = i + 1; j <= 57; j++)
+			{
+				putchar(c);
+				putchar(i);
+				putchar(j);
+				if (c != 55 && c != 56 && c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
