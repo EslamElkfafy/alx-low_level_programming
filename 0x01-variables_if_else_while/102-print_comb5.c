fdf3102808645c9884bbe4e14950ae0a807b3ad7
EslamElkfafy <eslamadel52@gmail.com>
2023-03-17 09:18:12 -0700

    first commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,30 +5,50 @@
  */
 int main(void)
 {
+	int digit0 = 0, digit1 = 0, digit2 = 0, digit3 = 0;
+	int init = 1;
 	int c;
-	int number, r;
-
-	for (c = 1; c <= 9899; c++)
+	for (c = 0; c <= 8549; c++)
 	{
-		number = c / 1000;
-		r = number % 10;
-		putchar(r + 48);
-		number = c / 100;
-		r = number % 10;
-		putchar(r + 48);
-		putchar(' ');
-		number = c / 10;
-		r = number % 10;
-		putchar(r + 48);
-		number = c;
-		r = number % 10;
-		putchar(r + 48);
-		if (c != 9899)
+   		digit0 += 1;
+   		putchar(digit3 + 48);
+   		putchar(digit2 + 48);
+   		putchar(' ');
+   		putchar(digit1 + 48);
+   		putchar(digit0 + 48);
+   		if (c != 8549)
 		{
 			putchar(',');
-			putchar(' ');
+   			putchar(' ');
 		}
+   		if (digit0 == 9 && digit1 == 9 && digit2 == 9)
+   		{
+       			digit3 += 1;
+       			digit2 = 0;
+       			digit1 = 0;
+       			digit0 = 0;
+   		}
+   		else if (digit0 == 9 && digit1 == 9)
+   		{
+       			digit2 +=1;
+       			digit1 = 0;
+       			if (init < 9)
+       			{
+	   			digit0 = init;
+	   			init += 1;
+       			}
+       			else
+       			{
+	   			init = 1;
+	   			digit0 = 0;
+       			}
+  		}
+   		else if (digit0 == 9)
+   		{
+			digit1 += 1;
+       			digit0 = 0;
+   		}
 	}
-	putchar('\n');
+	putchar('\n');         
 	return (0);
 }
