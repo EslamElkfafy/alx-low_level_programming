2682da551c79e5b9161a4e6f7c2527dbeb09b956
EslamElkfafy <eslamadel52@gmail.com>
2023-03-17 06:15:04 -0700

    first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,15 @@
 int main(void)
 {
 	int c;
-	int s = 44;
 
 	for (c = 48; c <= 57; c++)
 	{
 		if (c != 48)
 		{
+			putchar(',');
 			putchar(' ');
 		}
-		if (c == 57)
-		{
-			s -= 8;
-		}
 		putchar(c);
-		putchar(s);
 	}
 	putchar('\n');
 	return (0);
