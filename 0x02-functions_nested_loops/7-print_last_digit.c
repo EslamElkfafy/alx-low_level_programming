3ef14daeddd97dd784da73687920648b6bb2c019
EslamElkfafy <eslamadel52@gmail.com>
2023-03-21 05:43:54 -0700

    first commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,24 +8,21 @@
  */
 int print_last_digit(int number)
 {
-	int n;
+	int n = number % 10;
 
-	if (number > 0)
+	if (n > 0)
 	{
-		n = number % 10;
 		_putchar('0' + n);
 		return (n);
 	}
-	else if (number == 0)
-	{	
-		n = number % 10;
+	else if (n == 0)
+	{
 		_putchar('0' + n);
 		return (n);
 	}
 	else
 	{
-		number = number - 2 * number;
-		n = number % 10;
+		n *= -1
 		_putchar('0' + n);
 		return (n);
 	}
