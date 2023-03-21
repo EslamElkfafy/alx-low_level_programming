5f466c68efa38de471693016c5040db6ce9af6e1
EslamElkfafy <eslamadel52@gmail.com>
2023-03-21 11:54:50 -0700

    first commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,28 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n; n <= 98; n++)
+		for (n = n; n <= 98; n++)
 		{
 			if (n == 98)
 				printf("%i", n);
 			else
 				printf("%i, ", n);
 		}
+		_putchar('\n');
 	}
 	else if (n > 98)
 	{
-		for (n; n > 98; n--)
+		for (n = n; n >= 98; n--)
 		{
 			if (n == 98)
 				printf("%i", n);
 			else
 				printf("%i, ", n);
 		}
+		_putchar('\n');
 	}
 	else
 	{
-		printf("%i", 98);
+		printf("%i\n", 98);
 	}
 }
