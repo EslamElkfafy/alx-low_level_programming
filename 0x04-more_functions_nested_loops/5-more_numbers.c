3233c07a53382785bad26d9815b74795e6caa1ff
EslamElkafy <eslamadel52@gmail.com>
2023-03-23 15:34:08 +0200

    first commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14
- * _putchar only 3 times
- * Return: 0-14 x10 followed by new line
+ * more_numbers - prints 10 times the numbers,
+ *  from 0 to 14, followed by a new line.
  */
 void more_numbers(void)
 {
