fa01c077cd25d0ead619a47669826d587bdc6731
EslamElkafy <eslamadel52@gmail.com>
2023-03-27 13:24:43 +0200

    first commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *
+ * @s: The string
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	while (index--)
+	{
+		_putchar(s[index]);
+	}
+}
