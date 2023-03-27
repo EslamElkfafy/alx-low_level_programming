3c03ceb207b963a4702a3d19294ee9e85f9f7354
EslamElkafy <eslamadel52@gmail.com>
2023-03-27 17:09:14 +0200

    frist commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line.
+ *
+ * @a: the address of frist element
+ * @n: the number of elements of the array to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+        for (i = 0; i < n; i++)
+	{
+		if (i = 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+	}
+	putchar('\n');
+}
