5adad97337a25a72165fcb011fa422808da85c2e
EslamElkafy <eslamadel52@gmail.com>
2023-03-29 16:43:57 +0200

    first commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: the string
+ * Return: the string be updated
+ */
+char *cap_string(char *s)
+{
+	char *start = s;
+	int check = 0;
+
+	while (*start != '\0')
+	{
+		if (*start == ' ' ||
+		*start == '\t' ||
+		*start == '\n' ||
+		*start == ',' ||
+		*start == ';' ||
+		*start == '.' ||
+		*start == '!' ||
+		*start == '!' ||
+		*start == '?' ||
+		*start == '\"' ||
+		*start == '(' ||
+		*start == ')' ||
+		*start == '{' ||
+		*start == '}')
+		{
+			check = 1;
+		}
+		else
+		{
+			if (check == 1)
+                	{
+				if (*start > 'a' && *start < 'z')
+				{
+                        		*start -= 32;
+				}
+                	}
+			check = 0;
+		}
+		start++;
+	}
+	return (s);
+}
