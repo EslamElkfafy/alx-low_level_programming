91963abd7c816060f1c60e3041bb151e1c032a36
EslamElkafy <eslamadel52@gmail.com>
2023-04-04 01:33:55 +0200

    first commit

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,6 +1,4 @@
-#ifndef "main_h"
-#define "main_h"
-#include <stdio.h>
+
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -12,5 +10,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
