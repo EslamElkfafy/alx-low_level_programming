2d001777a6d738a4a72c607e9e6dbb8e44cf803e
EslamElkafy <eslamadel52@gmail.com>
2023-04-08 12:04:54 +0200

    first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (success)
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
