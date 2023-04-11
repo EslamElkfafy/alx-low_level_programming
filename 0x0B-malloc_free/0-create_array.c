8088cc46750907c43ffc4a740551dec589b34038
EslamElkafy <eslamadel52@gmail.com>
2023-04-11 12:51:05 +0200

    first commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,11 +19,14 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 		ar = malloc(sizeof(char) * size);
+		if (ar == NULL)
+		{
+			return (NULL);
+		}
 		for (i = 0; i < size; i++)
 		{
 			ar[i] = c;
 		}
-		ar[i] = '\0';
 		return (ar);
 	}
 }
