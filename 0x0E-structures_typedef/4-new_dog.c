937b9d5f10bf5761f6b41b49b638b6e54a55b412
EslamElkafy <eslamadel52@gmail.com>
2023-04-17 14:12:54 +0200

    first commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - detected length of string
+ * @s: the string
+ * Ruturn: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy of string
+ * @s1: first string
+ * @s2: secound string
+ * Return: string to be copied
+ */
+char *_strcpy(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s2[i] != '\0')
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+/**
+ * new_dog - creates a new dog.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	int len1, len2;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	new_dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+	return (new_dog);
+}
