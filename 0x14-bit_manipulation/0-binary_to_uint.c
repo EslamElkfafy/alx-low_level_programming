ff2ed9e484229cd2207a304d98e2a30f49485bad
EslamElkfafy <eslamadel52@gmail.com>
2024-02-11 20:47:00 +0200

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/*
+ * binary_to_uint - binary to unsigned integer
+ * @b: input binary
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b) {
+	int length = 0;
+	int sum = 0, i;
+	
+	if (b == NULL)
+	{
+		return (0);
+	}
+	i = 0;
+	while (b[i] != '\0')
+	{
+		i++;
+		length++;
+	}
+	for (i = 0; i < length ; i++)
+	{
+		if (b[i] != '1' && b[i] != 0)
+			return (0);
+		if (b[i] == '1')
+			sum += 1 << (length - i - 1);
+	}
+	return (sum);
+}
