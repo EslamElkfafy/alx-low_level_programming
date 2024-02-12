d10aed47098a2febca77f638df164fce59a0976a
EslamElkfafy <eslamadel52@gmail.com>
2024-02-12 16:49:25 +0200

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - that reads a text file and prints it
+ * @filename: the name of file
+ * @letters: number of letters
+ * Return: number of printed chars
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters){
+	FILE *file;
+	int character;
+	size_t counter = 0;
+	file = fopen(filename, "r");
+
+	if(file == NULL) {
+		return (0);
+	} 
+	while (((character = fgetc(file)) != EOF) && (counter != letters)) {
+		_putchar((char)character);
+		counter++;
+	}
+	fclose(file);
+	return(counter);
+}
