b7c0dc26584daed7ae243b68570a7a8bc2c487c5
EslamElkfafy <eslamadel52@gmail.com>
2024-02-14 12:15:49 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
-		status = write(file, "", 1);
+		status = write(file, "", 0);
 		close(file);
 		return (1);
 	}
-	for (text_size; text_content[text_size] != '\0'; text_size++)
+	for (text_size = 0; text_content[text_size] != '\0'; text_size++)
 		;
 	status = write(file, text_content, text_size);
+	if (status == -1)
+		return (-1);
 	close(file);
-	return (1)
+	return (1);
 }
